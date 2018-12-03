c9eb6d7a2dca4bbda219253f33df7b5e4bc3124d
shamin.meerankutty <shamin.meerankutty@fullcontact.com>
2018-12-03 22:50:29 +0530

    Replaced html-loader with html-srcsets-loader to compile src-set

--- webpack.config.js
+++ webpack.config.js
@@ -1,79 +1,85 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const webpack = require('webpack');
-const path = require('path');
-const globSync = require('glob').sync;
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const webpack = require("webpack");
+const path = require("path");
+const globSync = require("glob").sync;
 
-module.exports =  (env, options) => ({
-    entry: [
-        './src/index.js'
-    ],
-    devServer: {
-        contentBase: './dist'
-    },
-    devtool: "source-map",
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: [options.mode !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
-            },
-            {
-                test: /\.(png|jpg|gif)$/,
-                use: [{
-                    loader: 'file-loader',
-                    options: {
-                        name: '[name].[ext]',
-                        outputPath: 'img/'
-                    }
-                }]
-            },
-            {
-                test: /\.(html)$/,
-                use: {
-                    loader: 'html-loader',
-                    options: {
-                        attrs: [':src']
-                    }
-                }
-            },
-            {
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
-            }
+module.exports = (env, options) => ({
+  entry: ["./src/index.js"],
+  devServer: {
+    contentBase: "./dist"
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          options.mode !== "production"
+            ? "style-loader"
+            : MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader"
         ]
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: "css/[name].[contenthash].css",
-        }),
-        new CleanWebpackPlugin(["dist"]),
-        ...globSync('src/**/*.html').map((fileName) => {
-            return new HtmlWebpackPlugin({
-                template: fileName,
-                inject: "body",
-                filename: fileName.replace('src/', '')
-            })
-        }),
-        new webpack.ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery',
-            'window.jQuery': 'jquery',
-            Popper: ['popper.js', 'default'],
-            Util: "exports-loader?Util!bootstrap/js/dist/util",
-            Dropdown: "exports-loader?Dropdown!bootstrap/js/dist/dropdown",
-        })
-    ],
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: ''
-    }
-})
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "img/"
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(html)$/,
+        use: {
+          loader: "html-srcsets-loader",
+          options: {
+            attrs: [":src", ':srcset']
+          }
+        }
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/[name].[contenthash].css"
+    }),
+    new CleanWebpackPlugin(["dist"]),
+    ...globSync("src/**/*.html").map(fileName => {
+      return new HtmlWebpackPlugin({
+        template: fileName,
+        inject: "body",
+        filename: fileName.replace("src/", "")
+      });
+    }),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+      "window.jQuery": "jquery",
+      Popper: ["popper.js", "default"],
+      Util: "exports-loader?Util!bootstrap/js/dist/util",
+      Dropdown: "exports-loader?Dropdown!bootstrap/js/dist/dropdown"
+    })
+  ],
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: ""
+  }
+});
